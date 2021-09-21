66f84b40ddd38e1a619c2c14899172f80f522437
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-21 16:40:28 -0500

    Bugs corrected

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import {Switch, Route, Link} from 'react-router-dom'
-import {Layout, Typography, Space} from 'antd'
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components';
 
 const App: React.FC = () => {
 	return (
 		<div className="app">
 			<div className="navbar">
-		
+				<Navbar />
 			</div>
-			<div className="main">
-
-			</div>
-			<div className="footer">
-
-			</div>
+			<div className="main"></div>
+			<div className="footer"></div>
 		</div>
 	);
 };
