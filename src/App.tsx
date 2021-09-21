3f07c07e27d47ef39ca83bfd788b200ce3c79203
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-21 16:10:02 -0500

    Starting with navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom'
+import {Layout, Typography, Space} from 'antd'
 
-const App = () => {
+const App: React.FC = () => {
 	return (
-		<div>
-			<p></p>
-			<p></p>
+		<div className="app">
+			<div className="navbar">
+		
+			</div>
+			<div className="main">
+
+			</div>
+			<div className="footer">
+
+			</div>
 		</div>
 	);
 };
