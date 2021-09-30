c24e155ea3307faf1cfe33ec07a9f1ee3eb74163
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-30 12:16:48 -0500

    Crypto details completed

--- src/components/CryptoDetails.tsx
+++ src/components/CryptoDetails.tsx
@@ -29,61 +29,79 @@ const CryptoDetails = () => {
 
 	const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
-	const stats = [
-		{
-			title: 'Price to USD',
-			value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`,
-			icon: <DollarCircleOutlined />,
-		},
-		{ title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
-		{
-			title: '24h Volume',
-			value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`,
-			icon: <ThunderboltOutlined />,
-		},
-		{
-			title: 'Market Cap',
-			value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`,
-			icon: <DollarCircleOutlined />,
-		},
-		{
-			title: 'All-time-high(daily avg.)',
-			value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`,
-			icon: <TrophyOutlined />,
-		},
-	];
+	const stats = cryptoDetails
+		? [
+				{
+					title: 'Price to USD',
+					value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`,
+					icon: <DollarCircleOutlined />,
+				},
+				{ title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+				{
+					title: '24h Volume',
+					value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`,
+					icon: <ThunderboltOutlined />,
+				},
+				{
+					title: 'Market Cap',
+					value: `$ ${
+						cryptoDetails.marketCap && millify(cryptoDetails.marketCap)
+					}`,
+					icon: <DollarCircleOutlined />,
+				},
+				{
+					title: 'All-time-high(daily avg.)',
+					value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`,
+					icon: <TrophyOutlined />,
+				},
+		  ]
+		: [
+				{
+					title: 'No data available',
+					value: `Error fetching the data`,
+					icon: 'img',
+				},
+		  ];
 
-	const genericStats = [
-		{
-			title: 'Number Of Markets',
-			value: cryptoDetails.numberOfMarkets,
-			icon: <FundOutlined />,
-		},
-		{
-			title: 'Number Of Exchanges',
-			value: cryptoDetails.numberOfExchanges,
-			icon: <MoneyCollectOutlined />,
-		},
-		{
-			title: 'Aprroved Supply',
-			value: cryptoDetails.approvedSupply ? (
-				<CheckOutlined />
-			) : (
-				<StopOutlined />
-			),
-			icon: <ExclamationCircleOutlined />,
-		},
-		{
-			title: 'Total Supply',
-			value: `$ ${millify(cryptoDetails.totalSupply)}`,
-			icon: <ExclamationCircleOutlined />,
-		},
-		{
-			title: 'Circulating Supply',
-			value: `$ ${millify(cryptoDetails.circulatingSupply)}`,
-			icon: <ExclamationCircleOutlined />,
-		},
-	];
+	const genericStats = cryptoDetails
+		? [
+				{
+					title: 'Number Of Markets',
+					value: cryptoDetails.numberOfMarkets,
+					icon: <FundOutlined />,
+				},
+				{
+					title: 'Number Of Exchanges',
+					value: cryptoDetails.numberOfExchanges,
+					icon: <MoneyCollectOutlined />,
+				},
+				{
+					title: 'Aprroved Supply',
+					value: cryptoDetails.approvedSupply ? (
+						<CheckOutlined />
+					) : (
+						<StopOutlined />
+					),
+					icon: <ExclamationCircleOutlined />,
+				},
+				{
+					title: 'Total Supply',
+					value: `$ ${millify(cryptoDetails.totalSupply)}`,
+					icon: <ExclamationCircleOutlined />,
+				},
+				{
+					title: 'Circulating Supply',
+					value: `$ ${millify(cryptoDetails.circulatingSupply)}`,
+					icon: <ExclamationCircleOutlined />,
+				},
+		  ]
+		: [
+				{
+					title: 'No data available',
+					value: `Error fetching the data`,
+					icon: 'img',
+				},
+		  ];
 
 	if (isFetching) return <h2>Loading....</h2>;
 
@@ -147,6 +165,29 @@ const CryptoDetails = () => {
 							</Col>
 							<Text className="stats">{value}</Text>
 						</Col>
+					))}
+				</Col>
+			</Col>
+			<Col className="coin-desc-link">
+				<Row className="coin-desc">
+					<Title level={3} className="coin-details-heaindg">
+						What is {cryptoDetails.name}
+						{HTMLReactParser(cryptoDetails.description)}
+					</Title>
+				</Row>
+				<Col className="coin-links">
+					<Title level={3} className="coin-details-heading">
+						{cryptoDetails.name} Links
+					</Title>
+					{cryptoDetails.links.map((link: any) => (
+						<Row className="coin-link" key={link.name}>
+							<Title level={5} className="link-name">
+								{link.type}
+							</Title>
+							<a href={link.url} target="_blank" rel="noreferrer">
+								{link.name}
+							</a>
+						</Row>
 					))}
 				</Col>
 			</Col>
